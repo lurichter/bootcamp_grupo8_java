1150a2c957815db88b13bfcc2aa4069bb94913f4
Lucas Serrer Richter <lucas.richter@mercadolivre.com>
2021-05-21 18:57:34 -0300

    Exercício 8

---
+++ src/aula2/exec8/Main.java
@@ -0,0 +1,11 @@
+package aula2.exec8;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        WeakPassword senha = new WeakPassword("abeaweawaew");
+        MediumPassword senha2 = new MediumPassword("1ASjkdjs34");
+        StrongPassword senha3 = new StrongPassword("dasasd567!#");
+    }
+}
